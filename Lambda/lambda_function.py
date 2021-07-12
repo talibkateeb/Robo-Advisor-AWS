091ce2eb1d43f824f26bfa2183c3e3b8b14ddfa6
Talib Kateeb <talib.kateeb@gmail.com>
2021-07-11 19:56:15 -0700

    working bot, finished lambda function, and readme edits

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -80,38 +80,6 @@ def close(session_attributes, fulfillment_state, message):
     return response
 
 
-"""
-Step 3: Enhance the Robo Advisor with an Amazon Lambda Function
-
-In this section, you will create an Amazon Lambda function that will validate the data provided by the user on the Robo Advisor.
-
-1. Start by creating a new Lambda function from scratch and name it `recommendPortfolio`. Select Python 3.7 as runtime.
-
-2. In the Lambda function code editor, continue by deleting the AWS generated default lines of code, then paste in the starter code provided in `lambda_function.py`.
-
-3. Complete the `recommend_portfolio()` function by adding these validation rules:
-
-    * The `age` should be greater than zero and less than 65.
-    * The `investment_amount` should be equal to or greater than 5000.
-
-4. Once the intent is fulfilled, the bot should respond with an investment recommendation based on the selected risk level as follows:
-
-    * **none:** "100% bonds (AGG), 0% equities (SPY)"
-    * **low:** "60% bonds (AGG), 40% equities (SPY)"
-    * **medium:** "40% bonds (AGG), 60% equities (SPY)"
-    * **high:** "20% bonds (AGG), 80% equities (SPY)"
-
-> **Hint:** Be creative while coding your solution, you can have all the code on the `recommend_portfolio()` function, or you can split the functionality across different functions, put your Python coding skills in action!
-
-5. Once you finish coding your Lambda function, test it using the sample test events provided for this Challenge.
-
-6. After successfully testing your code, open the Amazon Lex Console and navigate to the `recommendPortfolio` bot configuration, integrate your new Lambda function by selecting it in the “Lambda initialization and validation” and “Fulfillment” sections.
-
-7. Build your bot, and test it with valid and invalid data for the slots.
-
-"""
-
-
 ### Intents Handlers ###
 def recommend_portfolio(intent_request):
     """
@@ -124,7 +92,49 @@ def recommend_portfolio(intent_request):
     risk_level = get_slots(intent_request)["riskLevel"]
     source = intent_request["invocationSource"]
 
-    # YOUR CODE GOES HERE!
+    # Validate if the user is between the age of 0 and 65
+    if (parse_int(age) < 0 or parse_int(age) >= 65):
+        return build_validation_result(
+            False,
+            "age",
+            "You must be younger than 65 years old and enter a valide age above 0,"
+            "Please provide a different age.",
+        )
+    
+    if (parse_int(investment_amount) < 5000):
+        return build_validation_result(
+            False,
+            "investmentAmount",
+            "You must invest at least 5000 dollars to continue,"
+            "Please enter a new amount you would like to invest",
+        )
+    
+    recommendation = analyze_risk(risk_level)
+
+    if (recommendation == 'Invalid Risk Level'):
+        return build_validation_result(
+            False,
+            "riskLevel",
+            "Please enter a valid risk level"
+            "None, Low, Medium, High",
+        )
+    
+    build_validation_result(True, None, recommendation)
+
+    
+def analyze_risk(risk_level):
+    if (risk_level == "None"):
+        recommendation = '100% bonds (AGG), 0% equities (SPY)'
+    elif (risk_level == "Low"):
+        recommendation = '60% bonds (AGG), 40% equities (SPY)'
+    elif (risk_level == "Medium"):
+        recommendation = '40% bonds (AGG), 60% equities (SPY)'
+    elif (risk_level == 'High'):
+        recommendation = '20% bonds (AGG), 80% equities (SPY)'
+    else:
+        recommendation = 'Invalid Risk Level'
+    return recommendation
+
 
 
 ### Intents Dispatcher ###
--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Robo-Advisor-AWS
+
+The goal of this project is to create an Amazon Lex bot with an Amazon Lambda function that gives investment advice for a retiremnet portfolio based on the users level of risk. 
+
+---
+
+## Working Bot Images and Videos
+
+The slots, sample utterances, and confirmation message:
+
+![]()
+
+![]()
+
+
+A video of the basic bot without the lambda function connected to it:
+
+![]()
+
+
+A video of the Lambda function being tested with test events:
+
+![]()
+
+A video of the working bot with the lambda function implemented:
+
+![]()
+
+## Technologies
+
+This application runs AWS using:
+
+* [Amazon Lex](https://aws.amazon.com/lex/) - A tool for creating conversational AI chatbots.
+
+* [Amazon Lambda](https://aws.amazon.com/lambda/)) - A tool for running code on the cloud. 
+
+---
+
+## Installation Guide
+
+[Sign up for AWS Services](https://aws.amazon.com/) 
+
+---
+
+## Contributors
+
+*  Talib Kateeb
+
+---
+
+## License
+
+[Click Here To View](https://github.com/talibkateeb/Robo-Advisor-AWS/blob/main/LICENSE)
